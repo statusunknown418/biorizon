861d4da41d2a20836b89b881f27f03cf2650cdc0
alvaro <alvarodevcode@outlook.com>
2024-04-08 12:38:49 -0500

    fix: deploy fix

--- src/components/pages/Header.tsx
+++ src/components/pages/Header.tsx
@@ -2,14 +2,16 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <nav className="sticky inset-0 flex w-full items-center justify-between bg-sky-800 p-3 text-white">
-      <h2 className="flex grow">Logo</h2>
+    <nav className="sticky inset-0 flex w-full items-center justify-center bg-sky-800 p-3 text-white">
+      <ul className="flex w-full max-w-5xl justify-between">
+        <h2 className="flex grow">Logo</h2>
 
-      <div className="flex gap-2">
-        <Link href={"#"}>Nav 1</Link>
-        <Link href={"#"}>Nav 2</Link>
-        <Link href={"#"}>Nav 3</Link>
-      </div>
+        <div className="flex gap-2">
+          <Link href={"#"}>Nav 1</Link>
+          <Link href={"#"}>Nav 2</Link>
+          <Link href={"#"}>Nav 3</Link>
+        </div>
+      </ul>
     </nav>
   );
 };
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -28,12 +28,6 @@ export const postRouter = createTRPCRouter({
       });
     }),
 
-  getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.query.posts.findFirst({
-      orderBy: (posts, { desc }) => [desc(posts.createdAt)],
-    });
-  }),
-
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
   }),
