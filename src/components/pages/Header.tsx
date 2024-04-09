bcf8f3b24a122544ba832f88064eab530aae6659
alvaro <alvarodevcode@outlook.com>
2024-04-09 11:38:41 -0500

    refactor: copy thing

--- src/components/pages/Header.tsx
+++ src/components/pages/Header.tsx
@@ -4,15 +4,15 @@ import { Button } from "../ui/button";
 export const Header = () => {
   return (
     <nav className="sticky inset-0 flex w-full items-center justify-center bg-primary p-3 text-primary-foreground">
-      <ul className="flex w-full max-w-5xl justify-between">
+      <ul className="flex w-full max-w-5xl items-center justify-between">
         <h2 className="flex grow">Logo</h2>
 
-        <div className="flex gap-2">
+        <div className="flex items-center gap-2">
           <Link href={"#"}>Webinars</Link>
-          <Link href={"#"}>About us</Link>
-          <Link href={"#"}>Support</Link>
-          <Link href={"#"}>Image gallery</Link>
-          <Link href={"#"}>Simulation</Link>
+          <Link href={"#"}>Nosotros</Link>
+          <Link href={"#"}>Soporte</Link>
+          <Link href={"#"}>Galeria</Link>
+          <Link href={"#"}>Simulacion</Link>
 
           <Button asChild>
             <Link href={"#"}>Login</Link>
