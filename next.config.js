d5d503b742c6d429daf160667036c99179f54554
alvaro <alvarodevcode@outlook.com>
2024-06-25 20:43:24 -0500

    feat: cors

--- next.config.js
+++ next.config.js
@@ -28,6 +28,26 @@ const config = {
       },
     ],
   },
+  headers: async () => {
+    return [
+      {
+        source: "/api/queues",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   pageExtensions: ["md", "mdx", "tsx", "ts", "js", "jsx"],
 };
 
